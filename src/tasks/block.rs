b4c64da68b4ff18d7a6d86554243ad0a812de768
dylan <dylan@init4.technology>
2025-04-28 12:59:25 -0600

    cleanup & refactors
    - adds newlines after arguments and returns sections in comments
    - pulls several functions out into methods

--- src/tasks/block.rs
+++ src/tasks/block.rs
@@ -51,11 +51,13 @@ impl Simulator {
     /// Creates a new `Simulator` instance.
     ///
     /// # Arguments
+    ///
     /// - `config`: The configuration for the builder.
     /// - `ru_provider`: A provider for interacting with the rollup.
     /// - `slot_calculator`: A slot calculator for managing block timing.
     ///
     /// # Returns
+    ///
     /// A new `Simulator` instance.
     pub fn new(
         config: &BuilderConfig,
@@ -68,11 +70,13 @@ impl Simulator {
     /// Handles building a single block.
     ///
     /// # Arguments
+    ///
     /// - `constants`: The system constants for the rollup.
     /// - `sim_items`: The simulation cache containing transactions and bundles.
     /// - `finish_by`: The deadline by which the block must be built.
     ///
     /// # Returns
+    ///
     /// A `Result` containing the built block or an error.
     pub async fn handle_build(
         &self,
@@ -104,89 +108,109 @@ impl Simulator {
     /// adding them to the simulation cache, and one to track the latest basefee.
     ///
     /// # Arguments
+    ///
     /// - `tx_receiver`: A channel receiver for incoming transactions.
     /// - `bundle_receiver`: A channel receiver for incoming bundles.
     /// - `cache`: The simulation cache to store the received items.
     ///
     /// # Returns
+    ///
     /// A `JoinHandle` for the basefee updater and a `JoinHandle` for the
     /// cache handler.
     pub fn spawn_cache_task(
         self: Arc<Self>,
-        mut tx_receiver: mpsc::UnboundedReceiver<TxEnvelope>,
-        mut bundle_receiver: mpsc::UnboundedReceiver<Bundle>,
+        tx_receiver: mpsc::UnboundedReceiver<TxEnvelope>,
+        bundle_receiver: mpsc::UnboundedReceiver<Bundle>,
         cache: SimCache,
     ) -> (JoinHandle<()>, JoinHandle<()>) {
         tracing::debug!("starting up cache handler");
 
-        let shared_price = Arc::new(AtomicU64::new(0_u64));
-        let price_updater = Arc::clone(&shared_price);
-        let price_reader = Arc::clone(&shared_price);
+        let basefee_price = Arc::new(AtomicU64::new(0_u64));
+        let basefee_reader = Arc::clone(&basefee_price);
 
         // Update the basefee on a per-block cadence
-        let basefee_jh = tokio::spawn(async move {
-            loop {
-                // calculate next slot position plus a small buffer to
-                let time_remaining = self.slot_calculator.slot_duration()
-                    - self.slot_calculator.current_timepoint_within_slot()
-                    + 1;
-                tracing::debug!(time_remaining = ?time_remaining, "sleeping until next slot");
-
-                // wait until that point in time
-                sleep(Duration::from_secs(time_remaining)).await;
-
-                // update the basefee with that price
-                tracing::debug!("checking latest basefee");
-                let resp = self.ru_provider.get_block_by_number(Latest).await;
-
-                if let Ok(maybe_block) = resp {
-                    match maybe_block {
-                        Some(block) => {
-                            let basefee = block.header.base_fee_per_gas.unwrap_or_default();
-                            price_updater.store(basefee, Ordering::Relaxed);
-                            tracing::debug!(basefee = %basefee, "basefee updated");
-                        }
-                        None => {
-                            tracing::debug!("no block found; basefee not updated.")
-                        }
-                    }
-                }
-            }
-        });
+        let basefee_jh =
+            tokio::spawn(async move { self.basefee_updater(Arc::clone(&basefee_price)).await });
 
         // Update the sim cache whenever a transaction or bundle is received with respect to the basefee
         let cache_jh = tokio::spawn(async move {
-            loop {
-                let p = price_reader.load(Ordering::Relaxed);
-                select! {
-                    maybe_tx = tx_receiver.recv() => {
-                        if let Some(tx) = maybe_tx {
-                            tracing::debug!(tx = ?tx.hash(), "received transaction");
-                            cache.add_item(SimItem::Tx(tx), p);
-                        }
-                    }
-                    maybe_bundle = bundle_receiver.recv() => {
-                        if let Some(bundle) = maybe_bundle {
-                            tracing::debug!(bundle = ?bundle.id, "received bundle");
-                            cache.add_item(SimItem::Bundle(bundle.bundle), p);
-                        }
-                    }
-                }
-            }
+            cache_updater(tx_receiver, bundle_receiver, cache, basefee_reader).await
         });
 
         (basefee_jh, cache_jh)
+    }
+
+    /// Periodically updates the shared basefee by querying the latest block.
+    ///
+    /// This function calculates the remaining time until the next slot,
+    /// sleeps until that time, and then retrieves the latest basefee from the rollup provider.
+    /// The updated basefee is stored in the provided `AtomicU64`.
+    ///
+    /// This function runs continuously.
+    ///
+    /// # Arguments
+    ///
+    /// - `price`: A shared `Arc<AtomicU64>` used to store the updated basefee value.
+    async fn basefee_updater(self: Arc<Self>, price: Arc<AtomicU64>) {
+        loop {
+            // calculate start of next slot plus a small buffer
+            let time_remaining = self.slot_calculator.slot_duration()
+                - self.slot_calculator.current_timepoint_within_slot()
+                + 1;
+            tracing::debug!(time_remaining = ?time_remaining, "basefee updater sleeping until next slot");
+
+            // wait until that point in time
+            sleep(Duration::from_secs(time_remaining)).await;
+
+            // update the basefee with that price
+            self.check_basefee(&price).await;
+        }
+    }
+
+    /// Queries the latest block from the rollup provider and updates the shared
+    /// basefee value if a block is found.
+    ///
+    /// This function retrieves the latest block using the provider, extracts the
+    /// `base_fee_per_gas` field from the block header (defaulting to zero if missing),
+    /// and updates the shared `AtomicU64` price tracker. If no block is available,
+    /// it logs a message without updating the price.
+    ///
+    /// # Arguments
+    ///
+    /// - `price`: A shared `Arc<AtomicU64>` used to store the updated basefee.
+    async fn check_basefee(&self, price: &Arc<AtomicU64>) {
+        tracing::debug!("checking latest basefee");
+        let resp = self.ru_provider.get_block_by_number(Latest).await;
+        if let Err(e) = resp {
+            tracing::debug!(err = %e, "basefee check failed with rpc error");
+            return;
+        }
+
+        if let Ok(maybe_block) = resp {
+            match maybe_block {
+                Some(block) => {
+                    let basefee = block.header.base_fee_per_gas.unwrap_or_default();
+                    price.store(basefee, Ordering::Relaxed);
+                    tracing::debug!(basefee = %basefee, "basefee updated");
+                }
+                None => {
+                    tracing::debug!("no block found; basefee not updated.")
+                }
+            }
+        }
     }
 
     /// Spawns the simulator task, which handles the setup and sets the deadline  
     /// for the each round of simulation.
     ///
     /// # Arguments
+    ///
     /// - `constants`: The system constants for the rollup.
     /// - `cache`: The simulation cache containing transactions and bundles.
     /// - `submit_sender`: A channel sender for submitting built blocks.
     ///
     /// # Returns
+    ///
     /// A `JoinHandle` for the spawned task.
     pub fn spawn_simulator_task(
         self: Arc<Self>,
@@ -195,28 +219,49 @@ impl Simulator {
         submit_sender: mpsc::UnboundedSender<BuiltBlock>,
     ) -> JoinHandle<()> {
         tracing::debug!("starting builder task");
-        tokio::spawn(async move {
-            loop {
-                let sim_cache = cache.clone();
-                let finish_by = self.calculate_deadline();
+        tokio::spawn(async move { self.run_simulator(constants, cache, submit_sender).await })
+    }
 
-                match self.handle_build(constants, sim_cache, finish_by).await {
-                    Ok(block) => {
-                        tracing::debug!(block = ?block, "built block");
-                        let _ = submit_sender.send(block);
-                    }
-                    Err(e) => {
-                        tracing::error!(err = %e, "failed to send block");
-                        continue;
-                    }
+    /// Continuously runs the block simulation and submission loop.
+    ///
+    /// This function clones the simulation cache, calculates a deadline for block building,
+    /// attempts to build a block using the latest cache and constants, and submits the built
+    /// block through the provided channel. If an error occurs during block building or submission,
+    /// it logs the error and continues the loop.
+    ///
+    /// This function runs indefinitely and never returns.
+    ///
+    /// # Arguments
+    /// - `constants`: The system constants for the rollup.
+    /// - `cache`: The simulation cache containing transactions and bundles.
+    /// - `submit_sender`: A channel sender used to submit built blocks.
+    async fn run_simulator(
+        self: Arc<Self>,
+        constants: SignetSystemConstants,
+        cache: SimCache,
+        submit_sender: mpsc::UnboundedSender<BuiltBlock>,
+    ) {
+        loop {
+            let sim_cache = cache.clone();
+            let finish_by = self.calculate_deadline();
+
+            match self.handle_build(constants, sim_cache, finish_by).await {
+                Ok(block) => {
+                    tracing::debug!(block = ?block, "built block");
+                    let _ = submit_sender.send(block);
+                }
+                Err(e) => {
+                    tracing::error!(err = %e, "failed to send block");
+                    continue;
                 }
             }
-        })
+        }
     }
 
     /// Calculates the deadline for the current block simulation.
     ///
     /// # Returns
+    ///
     /// An `Instant` representing the deadline.
     pub fn calculate_deadline(&self) -> Instant {
         let now = SystemTime::now();
@@ -228,6 +273,7 @@ impl Simulator {
     /// Creates an `AlloyDB` instance from the rollup provider.
     ///
     /// # Returns
+    ///
     /// An `Option` containing the wrapped database or `None` if an error occurs.
     async fn create_db(&self) -> Option<WrapAlloyDatabaseAsync> {
         let latest = match self.ru_provider.get_block_number().await {
@@ -242,6 +288,43 @@ impl Simulator {
             panic!("failed to acquire async alloy_db; check which runtime you're using")
         });
         Some(wrapped_db)
+    }
+}
+
+/// Continuously updates the simulation cache with incoming transactions and bundles.
+///
+/// This function listens for new transactions and bundles on their respective
+/// channels and adds them to the simulation cache using the latest observed basefee.
+///
+/// # Arguments
+/// - `tx_receiver`: A receiver channel for incoming Ethereum transactions.
+/// - `bundle_receiver`: A receiver channel for incoming transaction bundles.
+/// - `cache`: The simulation cache used to store transactions and bundles.
+/// - `price_reader`: An `Arc<AtomicU64>` providing the latest basefee for simulation pricing.
+async fn cache_updater(
+    mut tx_receiver: mpsc::UnboundedReceiver<
+        alloy::consensus::EthereumTxEnvelope<alloy::consensus::TxEip4844Variant>,
+    >,
+    mut bundle_receiver: mpsc::UnboundedReceiver<Bundle>,
+    cache: SimCache,
+    price_reader: Arc<AtomicU64>,
+) -> ! {
+    loop {
+        let p = price_reader.load(Ordering::Relaxed);
+        select! {
+            maybe_tx = tx_receiver.recv() => {
+                if let Some(tx) = maybe_tx {
+                    tracing::debug!(tx = ?tx.hash(), "received transaction");
+                    cache.add_item(SimItem::Tx(tx), p);
+                }
+            }
+            maybe_bundle = bundle_receiver.recv() => {
+                if let Some(bundle) = maybe_bundle {
+                    tracing::debug!(bundle = ?bundle.id, "received bundle");
+                    cache.add_item(SimItem::Bundle(bundle.bundle), p);
+                }
+            }
+        }
     }
 }
 
