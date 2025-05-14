2fd44a7024d20ed2779f0032335ab38035d7498a
James <james@prestwi.ch>
2025-05-14 13:39:59 -0400

    refactor: integrate env and cache tasks

--- tests/bundle_poller_test.rs
+++ tests/bundle_poller_test.rs
@@ -1,17 +1,15 @@
-mod tests {
-    use builder::test_utils;
-    use eyre::Result;
+use builder::test_utils;
+use eyre::Result;
 
-    #[ignore = "integration test"]
-    #[tokio::test]
-    async fn test_bundle_poller_roundtrip() -> Result<()> {
-        let config = test_utils::setup_test_config().unwrap();
-        let token = config.oauth_token();
+#[ignore = "integration test"]
+#[tokio::test]
+async fn test_bundle_poller_roundtrip() -> Result<()> {
+    let config = test_utils::setup_test_config().unwrap();
+    let token = config.oauth_token();
 
-        let mut bundle_poller = builder::tasks::cache::BundlePoller::new(&config, token);
+    let mut bundle_poller = builder::tasks::cache::BundlePoller::new(&config, token);
 
-        let _ = bundle_poller.check_bundle_cache().await?;
+    let _ = bundle_poller.check_bundle_cache().await?;
 
-        Ok(())
-    }
+    Ok(())
 }
