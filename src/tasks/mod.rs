9be8e0fe195ff2c1c649797800cc402a32aef423
James <james@prestwi.ch>
2025-05-14 13:56:07 -0400

    refactor: cache directory

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -4,9 +4,6 @@ pub mod block;
 /// Cache ingestion task
 pub mod cache;
 
-/// Bundle poller task
-pub mod bundler;
-
 /// Tx submission metric task
 pub mod metrics;
 
@@ -15,9 +12,6 @@ pub mod oauth;
 
 /// Tx submission task
 pub mod submit;
-
-/// Tx polling task
-pub mod tx_poller;
 
 /// Constructs the simualtion environment.
 pub mod env;
