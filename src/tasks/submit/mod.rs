5198d4fd0a486441f455c7068b18ab227c5677a3
dylan <dylan@init4.technology>
2025-08-13 14:23:14 -0600

    wip: adds Flashbots provider
    - adds a FlashbotsProvider for interacting with Flashbots API
    - adds smoke tests for sim and send bundle Flashbots endpoints
    - adds a make target for running the Flashbots integration tests

--- src/tasks/submit/mod.rs
+++ src/tasks/submit/mod.rs
@@ -4,5 +4,8 @@ pub use prep::{Bumpable, SubmitPrep};
 mod sim_err;
 pub use sim_err::{SimErrorResp, SimRevertKind};
 
-mod builder_helper;
+pub mod builder_helper;
 pub use builder_helper::{BuilderHelperTask, ControlFlow};
+
+pub mod flashbots;
+pub use flashbots::{FlashbotsSubmitter, FlashbotsTask};
