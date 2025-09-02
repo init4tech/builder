8505fc502ab3daecb119eda1a548fab4b3a9af20
dylan <dylan@init4.technology>
2025-09-02 12:59:06 -0600

    debugging and auth updates

--- tests/flashbots_provider_test.rs
+++ tests/flashbots_provider_test.rs
@@ -2,52 +2,32 @@
 //! These tests require the `FLASHBOTS_ENDPOINT` env var to be set.
 
 use alloy::{
-    primitives::FixedBytes,
-    rpc::types::mev::{EthBundleHash, MevSendBundle},
+    eips::Encodable2718,
+    primitives::U256,
+    rpc::types::mev::{BundleItem, MevSendBundle, ProtocolVersion},
+    signers::local::PrivateKeySigner,
 };
-use builder::tasks::submit::flashbots::FlashbotsProvider;
-use builder::test_utils::{setup_logging, setup_test_config};
+use builder::tasks::submit::flashbots::Flashbots;
+use builder::test_utils::{new_signed_tx, setup_logging, setup_test_config};
 
 #[tokio::test]
 #[ignore = "integration test"]
-async fn smoke_root_provider() {
+async fn simulate_valid_bundle() {
     setup_logging();
-    let flashbots = get_test_provider().await;
-    assert_eq!(flashbots.relay_url.as_str(), "http://localhost:9062/");
 
-    let status =
-        flashbots.bundle_status(EthBundleHash { bundle_hash: FixedBytes::default() }, 0).await;
-    assert!(status.is_err());
-}
-
-#[tokio::test]
-#[ignore = "integration test"]
-async fn smoke_simulate_bundle() {
     let flashbots = get_test_provider().await;
 
-    let res = flashbots.simulate_bundle(MevSendBundle::default()).await;
+    let wallet = PrivateKeySigner::random();
+    let tx = new_signed_tx(&wallet, 0, U256::from(1u64), 51_000).unwrap();
+    let tx_bytes = tx.encoded_2718().into();
 
-    if let Err(err) = &res {
-        let msg = format!("{err}");
-        assert!(msg.contains("mev_simBundle"));
-    }
-    assert!(res.is_err());
+    let bundle_body = vec![BundleItem::Tx { tx: tx_bytes, can_revert: false }];
+    let bundle = MevSendBundle::new(0, Some(0), ProtocolVersion::V0_1, bundle_body);
+
+    let _ = flashbots.simulate_bundle(bundle).await.expect("failed to simulate bundle");
 }
 
-#[tokio::test]
-#[ignore = "integration test"]
-async fn smoke_send_bundle() {
-    let flashbots = get_test_provider().await;
-    let res = flashbots.send_bundle(MevSendBundle::default()).await;
-
-    if let Err(err) = &res {
-        let msg = format!("{err}");
-        assert!(msg.contains("mev_sendBundle"));
-    }
-    assert!(res.is_ok() || res.is_err());
-}
-
-async fn get_test_provider() -> FlashbotsProvider {
+async fn get_test_provider() -> Flashbots {
     let config = setup_test_config().unwrap();
-    FlashbotsProvider::new(&config.clone())
+    Flashbots::new(&config.clone()).await
 }
