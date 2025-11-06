f6321f9a8460162ca8b7a438db52c7ccfec80730
dylan <dylan@init4.technology>
2025-11-06 13:58:42 -0700

    host env and rollup env fixes
    - properly builds a cfg env and block env from prev host header
    - makes max host gas a configurable property
    - cleans up the build prep in the sim loop

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -58,6 +58,7 @@ pub fn setup_test_config() -> Result<BuilderConfig> {
             1740681556, // pecorino start timestamp as sane default
             0, 1,
         ),
+        max_host_gas_coefficient: Some(80),
         constants: SignetSystemConstants::pecorino(),
     };
     Ok(config)
