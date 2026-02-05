51fcf216d9c19620a79a50871278af37ec95d497
evalir <e@evalir.xyz>
2026-02-05 15:10:05 +0100

    feat(submit): pylon blob preloading integration

--- src/tasks/submit/mod.rs
+++ src/tasks/submit/mod.rs
@@ -5,5 +5,9 @@ pub use flashbots::FlashbotsTask;
 mod prep;
 pub use prep::{Bumpable, SubmitPrep};
 
+/// Submission logic for Pylon blob server
+pub mod pylon;
+pub use pylon::PylonTask;
+
 mod sim_err;
 pub use sim_err::{SimErrorResp, SimRevertKind};
