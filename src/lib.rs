bdd40d9100d09d8f1f803196b088ed98411b6a86
Fraser Hutchison <190532+Fraser999@users.noreply.github.com>
2026-02-06 22:40:21 +0000

    chore: update to match changed dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![recursion_limit = "256"]
 
 #[macro_use]
 mod macros;
